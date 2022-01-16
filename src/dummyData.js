2f2e54163ea2db5e8c059a360e68085a41b04b43
“harzjunior” <harunjibs@gmail.com>
2022-01-16 23:33:44 +0300

    userList Component and table data

--- src/dummyData.js
+++ src/dummyData.js
@@ -1,3 +1,5 @@
+import profileImg from "./images/assets/profile-img.png";
+
 export const userData = [
   {
     name: "Jan",
@@ -70,5 +72,199 @@ export const userData = [
     activeUser: 4000,
     //   pv: 4300,
     //   amt: 2100,
+  },
+];
+
+//define data entries in rows
+export const userRows = [
+  {
+    id: "1",
+    username: "John_Doe",
+    avatar: profileImg,
+    email: "john-doe@email.com",
+    status: "active",
+    transaction: "$192.00",
+  },
+  {
+    id: "2",
+    username: "Jane_Doe",
+    avatar: profileImg,
+
+    email: "jane-doe@email.com",
+    status: "active",
+    transaction: "$158.00",
+  },
+  {
+    id: "3",
+    username: "John_Smith",
+    avatar: profileImg,
+
+    email: "john-smith@email.com",
+    status: "active",
+    transaction: "$245.00",
+  },
+  {
+    id: "4",
+    username: "Jane_Smith",
+    avatar: profileImg,
+
+    email: "jane-smith@email.com",
+    status: "active",
+    transaction: "$220.00",
+  },
+  {
+    id: "5",
+    username: "John_Doe",
+    avatar: profileImg,
+
+    email: "jane-doe@email.com",
+    status: "active",
+    transaction: "$110.00",
+  },
+  {
+    id: "6",
+    username: "Jane_Doe",
+    avatar: profileImg,
+
+    email: "jane-doe@email.com",
+    status: "active",
+    transaction: "$129.00",
+  },
+  {
+    id: "7",
+    username: "John_Smith",
+    avatar: profileImg,
+
+    email: "jhon-smith@email.com",
+    status: "active",
+    transaction: "$229.00",
+  },
+  {
+    id: "8",
+    username: "Jane_Smith",
+    avatar: profileImg,
+
+    email: "jane-doe@email.com",
+    status: "active",
+    transaction: "$132.00",
+  },
+  {
+    id: "9",
+    username: "John_Doe",
+    avatar: profileImg,
+
+    email: "jane-doe@email.com",
+    status: "active",
+    transaction: "$111.00",
+  },
+  {
+    id: "10",
+    username: "Jane_Doe",
+    avatar: profileImg,
+
+    email: "jane-doe@email.com",
+    status: "active",
+    transaction: "$121.00",
+  },
+  {
+    id: "11",
+    username: "John_Smith",
+    avatar: profileImg,
+
+    email: "jane-doe@email.com",
+    status: "active",
+    transaction: "$148.00",
+  },
+  {
+    id: "12",
+    username: "Jane_Smith",
+    avatar: profileImg,
+
+    email: "jane-doe@email.com",
+    status: "active",
+    transaction: "$216.00",
+  },
+  {
+    id: 13,
+    username: "Lannister_Cersei",
+    avatar: profileImg,
+
+    email: "jane-doe@email.com",
+    status: "active",
+    transaction: "$212.00",
+  },
+  {
+    id: 14,
+    username: "Lannister_Jaime",
+    avatar: profileImg,
+
+    email: "jane-doe@email.com",
+    status: "active",
+    transaction: "$300.00",
+  },
+  {
+    id: 15,
+    username: "Stark_Arya",
+    avatar: profileImg,
+
+    email: "jane-doe@email.com",
+    status: "active",
+    transaction: "$209.00",
+  },
+  {
+    id: 16,
+    username: "Targaryen_Daenerys",
+
+    avatar: profileImg,
+
+    email: "jane-doe@email.com",
+    status: "active",
+    transaction: "$211.00",
+  },
+  {
+    id: 17,
+    username: "Melisandre",
+
+    avatar: profileImg,
+
+    email: "jane-doe@email.com",
+    status: "active",
+    transaction: "$105.00",
+  },
+  {
+    id: 18,
+    username: "Clifford_Ferrara",
+    avatar: profileImg,
+
+    email: "jane-doe@email.com",
+    status: "active",
+    transaction: "$112.00",
+  },
+  {
+    id: 19,
+    username: "Frances_Rossini",
+    avatar: profileImg,
+
+    email: "jane-doe@email.com",
+    status: "active",
+    transaction: "$137.00",
+  },
+  {
+    id: 20,
+    username: "Roxie_Harvey",
+    avatar: profileImg,
+
+    email: "jane-doe@email.com",
+    status: "active",
+    transaction: "$124.00",
+  },
+  {
+    id: 21,
+    username: "Snow_Jon",
+    avatar: profileImg,
+
+    email: "jane-doe@email.com",
+    status: "active",
+    transaction: "$188.00",
   },
 ];
--- src/pages/userList/UserList.jsx
+++ src/pages/userList/UserList.jsx
@@ -1,123 +1,118 @@
 import "./userList.css";
 import { DataGrid } from "@material-ui/data-grid";
-
-//fine our columns using mui-datagrid
-const columns = [
-  { field: "id", headerName: "ID", width: 70 },
-  { field: "firstName", headerName: "First name", width: 130 },
-  { field: "lastName", headerName: "Last name", width: 130 },
-  //define age
-  {
-    field: "age",
-    headerName: "Age",
-    type: "number",
-    width: 90,
-  },
-  //define names
-  {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    // valueGetter: (params) =>
-    //   `${params.getValue(params.id, "firstName") || ""} ${
-    //     params.getValue(params.id, "lastName") || ""
-    //   }`,
-  },
-];
-//end of columns
-//define data entries in rows
-const rows = [
-  {
-    id: "1",
-    firstName: "John",
-    lastName: "Doe",
-    age: 32,
-  },
-  {
-    id: "2",
-    firstName: "Jane",
-    lastName: "Doe",
-    age: 28,
-  },
-  {
-    id: "3",
-    firstName: "John",
-    lastName: "Smith",
-    age: 25,
-  },
-  {
-    id: "4",
-    firstName: "Jane",
-    lastName: "Smith",
-    age: 22,
-  },
-  {
-    id: "5",
-    firstName: "John",
-    lastName: "Doe",
-    age: 32,
-  },
-  {
-    id: "6",
-    firstName: "Jane",
-    lastName: "Doe",
-    age: 28,
-  },
-  {
-    id: "7",
-    firstName: "John",
-    lastName: "Smith",
-    age: 25,
-  },
-  {
-    id: "8",
-    firstName: "Jane",
-    lastName: "Smith",
-    age: 22,
-  },
-  {
-    id: "9",
-    firstName: "John",
-    lastName: "Doe",
-    age: 32,
-  },
-  {
-    id: "10",
-    firstName: "Jane",
-    lastName: "Doe",
-    age: 28,
-  },
-  {
-    id: "11",
-    firstName: "John",
-    lastName: "Smith",
-    age: 25,
-  },
-  {
-    id: "12",
-    firstName: "Jane",
-    lastName: "Smith",
-    age: 22,
-  },
-  { id: 13, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 14, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 15, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 16, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 17, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 18, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 19, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 20, lastName: "Roxie", firstName: "Harvey", age: 65 },
-  { id: 21, lastName: "Snow", firstName: "Jon", age: 35 },
-];
-//end of data entries
+import { userRows } from "../../dummyData";
+import { useState } from "react";
+import { DeleteOutline } from "@material-ui/icons";
+import { Link } from "react-router-dom";
 
 export default function UserList() {
+  //fine our columns using mui-datagrid
+  const userColumns = [
+    // id colmun
+    {
+      field: "id",
+      headerName: "ID",
+      width: 95,
+    },
+    // username colmun renderCell function to render the avatar and the username
+    {
+      field: "user",
+      headerName: "User",
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <div className="user__list-user">
+            <img
+              src={params.row.avatar}
+              alt="avatar"
+              className="user__list-image"
+              // style={{ width: "30px", height: "30px" }}
+            />
+            {params.row.username}
+          </div>
+        );
+      },
+    },
+    // email colmun
+    {
+      field: "email",
+      headerName: "Email",
+      width: 170,
+    },
+    // email colmun
+    {
+      field: "status",
+      headerName: "Status",
+      width: 120,
+    },
+    //transaction colmun
+    {
+      field: "transaction",
+      headerName: "Transaction",
+      width: 160,
+    },
+    //action columns and renderCell function to render the action buttons edit and delete
+    {
+      field: "action",
+      headerName: "Action",
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <>
+            {/* create a link button that links to user page with their ID inlcuded */}
+            <Link to={"/user/" + params.row.id} className="btn btn-link">
+              <button className="user__list-edit ">Edit</button>
+            </Link>
+            <DeleteOutline
+              className="user__list-deleteIcon"
+              //if user id is available then call the deleteUser function
+              onClick={() => {
+                if (params.row.id) {
+                  deleteUser(params.row.id);
+                }
+              }}
+            />
+          </>
+        );
+      },
+    },
+    //
+    // full names column
+    // {
+    //   field: "fullName",
+    //   headerName: "Full name",
+    //   description: "This column has a value getter and is not sortable.",
+    //   sortable: false,
+    //   width: 160,
+    // get username and lastName by id and concat them together else return null
+    //   valueGetter: (params) =>
+    //     `${params.getValue(params.id, "username") || ""}
+    //     ${params.getValue(params.id, "lastName") || ""}
+    //     `,
+    // },
+  ];
+  //end of columns
+
+  //a function that delete a user from the list when the delete button is clicked with useState hook
+  const [data, setData] = useState(userRows);
+  //create a delete function that will delete a user from the list and send it to dummyData.js
+  const deleteUser = (id) => {
+    const newData = data.filter((userItem) => userItem.id !== id);
+    setData(newData);
+  };
+
   return (
     <div className="user__list">
-      {/* call the data grid */}
-      <DataGrid rows={rows} columns={columns} pageSize={10} checkboxSelection />
+      {/* call the data grid and disable selection of box when click on item (disableSelectionOnClick)*/}
+      <DataGrid
+        //use userRows and userColumns from dummyData
+        //instead of using the userRows we can use the data (default = userRows) from the useState hook
+        rows={data}
+        columns={userColumns}
+        pageSize={10}
+        disableSelectionOnClick
+        checkboxSelection
+      />
     </div>
   );
 }
